ec97d8ed5ae1855d34a9b2e6b991187d29286616
cj <cjlazell@gmail.com>
2014-02-20 18:54:42 -0600

    fix livereload to only work in dev mode

--- app.rb
+++ app.rb
@@ -8,6 +8,11 @@ require './plugins/enviroment'
 # rack
 require "rack/protection"
 
+if ENV['RACK_ENV'] == 'development'
+  require 'rack-livereload'
+  Cuba.use Rack::LiveReload
+end
+
 # midleware
 Cuba.use Rack::Session::Cookie, :secret => "__a_very_long_string__"
 Cuba.use Rack::Protection
@@ -15,12 +20,11 @@ Cuba.use Rack::Protection
 Cuba.plugin Cuba::Render
 Cuba.plugin Environment
 
-Cuba.define do
-  on development? do
-    require 'rack-livereload'
-    Cuba.use Rack::LiveReload
-  end
+# midleware
+Cuba.use Rack::Session::Cookie, :secret => "__a_very_long_string__"
+Cuba.use Rack::Protection
 
+Cuba.define do
   on get do
     on "hello" do
       res.write render("views/hello.haml")
